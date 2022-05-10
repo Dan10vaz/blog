93e34c83f478d274b63743a522b2ab1e05f6102b
Daniel Vaz <dcva9565@hotmail.com>
2022-05-09 23:55:14 -0500

    first commit de curso laravel

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+@section('title', 'Cursos create')
+
+@section('content')
+    <h2>En esta pagina podras crear un curso</h2>
+@endsection
---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantilla')
+@section('title', 'Cursos')
+
+@section('content')
+    <h1>Hola bienvneido a la pagina principal de cursos</h1>
+    <ul>
+        @foreach ($cursos as $curso)
+            <li>{{ $curso->name }}</li>
+        @endforeach
+    </ul>
+@endsection
